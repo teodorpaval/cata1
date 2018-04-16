fb1209cabbba1e0aa355cb6b83308ca2bef3b8f0
teopaval <teopaval@gmail.com>
2018-04-16 19:23:48 +0300

    code cleanup

--- parser.py
+++ parser.py
@@ -1,10 +1,11 @@
-def splitter (x, y):
+def splitter(x, y):
     return x.split(y)
 
-def parser (x):
-    data = splitter(x,"=")
+
+def parser(x):
+    data = splitter(x, "=")
     result = []
-    for i in range(0,len(data),1):
+    for i in range(0, len(data), 1):
         result.append(int(data[i]))
     return result
 
