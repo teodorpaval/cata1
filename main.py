3e8de27795b31f8cab157d09e1d7555c9c292edd
teopaval <teopaval@gmail.com>
2018-04-16 21:26:40 +0300

    level 4

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import solver
 
-
-left = 6
-right = 0
-print(solver.assembler(left, right))
+left = [9, 2, -5, 7]
+right = [8, -2]
+#print(solver.solve_1(left, right))
+print(solver.sum_lists(left, right)[0])
+print(solver.sum_lists(left, right)[1])
+print(solver.sum_lists(left, right)[2])
+print(solver.sum_lists(left, right)[3])
--- solver.py
+++ solver.py
@@ -1,19 +1,53 @@
-def get_table():
-    table = {0: [6, 9], 1: [], 2: [3], 3: [2, 5], 4: [], 5: [3], 6: [0, 9], 7: [], 8: [], 9: [0, 6]}
-    return table
-
+table_move = {0: [6, 9], 2: [3], 3: [2, 5], 5: [3], 6: [0, 9], 9: [0, 6],
+              -2: [-3], -3: [-2, -5], -5: [-3], -6: [0, -9], -9: [0, -6]}
+table_add = [[8], [7], [], [9], [], [6, 9], [8], [], [], [8]]
+table_remove = [[], [], [], [], [], [], [5], [1], [0, 6, 9], [3, 5]]
 
 def solve_1(left, right):
-    table = get_table()
 
-    if right in table[left]:
-        right = left
-    elif left in table[right]:
-        left = right
+    result = move(left, right)
+    if len(result) > 0:
+        return result
+    else:
+        result = move(right, left)
+        if len(result) > 0:
+            return result
 
     return left, right
 
 
-def assembler(left, right):
-    result = solve_1(left, right)
-    return str(result[0]) + "=" + str(result[1])
+def move(left, right):
+
+    for i in range(0, len(left)):
+        if left[i] in table_move.keys():
+            aux = left[i]
+            for j in table_move[aux]:
+                left[i] = j
+                if sum(left) == sum(right):
+                    return left, right
+            left[i] = aux
+
+    return []
+
+
+def differences (l, t):
+    res = {}
+    for i in l:
+        for j in t[abs(i)]:
+            if i < 0:
+                diff = -j - i
+                res[diff] = [i, -j]
+            else:
+                diff = j - i
+                res[diff] = [i, j]
+    return res
+
+
+def sum_lists(left, right):
+
+    left_remove = differences(left, table_remove)
+    left_add = differences(left, table_add)
+    right_remove = differences(right, table_remove)
+    right_add = differences(right, table_add)
+
+    return left_remove, left_add, right_remove, right_add
